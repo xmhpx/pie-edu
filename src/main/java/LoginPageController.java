39efd2d4d34ec05a71e6d7e8255115c8dec3c94e
xmhpx <mhppaydar@gmail.com>
2022-04-06 15:39:06 +0430

    added buttons and textfields and captcha to loginPage.fxml and LoginPageController

--- src/main/java/LoginPageController.java
+++ src/main/java/LoginPageController.java
@@ -1,3 +1,22 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
 public class LoginPageController {
+
+    @FXML
+    Text errorText;
+    @FXML
+    TextField usernameTextfield;
+    @FXML
+    TextField passwordTextfield;
+    @FXML
+    ImageView captchaImage;
+    @FXML
+    TextField captchaTextfield;
+    @FXML
+    Button LoginButton;
 
 }
