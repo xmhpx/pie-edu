1e71f127136e533ac4b6deeee363e08c614ad699
xmhpx <mhppaydar@gmail.com>
2022-04-09 19:04:42 +0430

    added teacherNumber and teacherLevel to Teacher

--- src/main/java/models/Teacher.java
+++ src/main/java/models/Teacher.java
@@ -1,4 +1,27 @@
 package models;
 
-public class Teacher {
+public class Teacher extends User{
+    protected String teacherNumber;
+    protected TeacherLevel teacherLevel;
+
+
+
+    // getters and setters
+
+    public String getTeacherNumber() {
+        return teacherNumber;
+    }
+
+    public void setTeacherNumber(String teacherNumber) {
+        this.teacherNumber = teacherNumber;
+    }
+
+
+    public TeacherLevel getTeacherLevel() {
+        return teacherLevel;
+    }
+
+    public void setTeacherLevel(TeacherLevel teacherLevel) {
+        this.teacherLevel = teacherLevel;
+    }
 }
