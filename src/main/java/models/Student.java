e868e14de7a2b8410690747467e593542daacf3d
xmhpx <mhppaydar@gmail.com>
2022-04-09 18:05:57 +0430

    Added Student.java

--- src/main/java/models/Student.java
+++ src/main/java/models/Student.java
@@ -1,4 +1,86 @@
 package models;
 
-public class Student {
+import java.util.ArrayList;
+
+public class Student extends User{
+    protected String educationStatus;
+    protected Teacher supervisor;
+    protected String registrationLicense;
+    protected String registrationTime;
+
+
+    protected ArrayList<ReportCard> reportCards;
+    protected ArrayList<Request> requests;
+
+    // getters and setters
+
+    public String getEducationStatus() {
+        return educationStatus;
+    }
+
+    public void setEducationStatus(String educationStatus) {
+        this.educationStatus = educationStatus;
+    }
+
+
+    public Teacher getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(Teacher supervisor) {
+        this.supervisor = supervisor;
+    }
+
+
+    public String getRegistrationLicense() {
+        return registrationLicense;
+    }
+
+    public void setRegistrationLicense(String registrationLicense) {
+        this.registrationLicense = registrationLicense;
+    }
+
+
+    public String getRegistrationTime() {
+        return registrationTime;
+    }
+
+    public void setRegistrationTime(String registrationTime) {
+        this.registrationTime = registrationTime;
+    }
+
+
+    public ArrayList<ReportCard> getReportCards() {
+        return reportCards;
+    }
+
+    public void setReportCards(ArrayList<ReportCard> reportCards) {
+        this.reportCards = reportCards;
+    }
+
+    public void addReportCards(ReportCard reportCard){
+        reportCards.add(reportCard);
+    }
+
+    public void removeReportCards(ReportCard reportCard){
+        reportCards.remove(reportCard);
+    }
+
+
+    public ArrayList<Request> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(ArrayList<Request> requests) {
+        this.requests = requests;
+    }
+
+    public void addReportCards(Request request){
+        requests.add(request);
+    }
+
+    public void removeReportCards(Request request){
+        requests.remove(request);
+    }
+
 }
