8942f9d043b95f61e5ba7973a0d2fb42aee38103
xmhpx <mhppaydar@gmail.com>
2022-04-15 23:14:03 +0430

    added studentHomePage.fxml

--- src/main/java/GuiMain.java
+++ src/main/java/GuiMain.java
@@ -11,7 +11,7 @@ public class GuiMain extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("loginPage.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("studentHomePage.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root);
