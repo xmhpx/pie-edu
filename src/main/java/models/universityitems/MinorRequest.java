a811b42aa80a56c3588600f19a9904e7f7c34b91
xmhpx <mhppaydar@gmail.com>
2022-04-18 21:09:12 +0430

    added class 'MinorRequest'

---
+++ src/main/java/models/universityitems/MinorRequest.java
@@ -0,0 +1,38 @@
+package models.universityitems;
+
+import models.student.Student;
+
+import java.lang.ref.Reference;
+
+public class MinorRequest extends Request{
+    protected Reference<College> originCollegeReference;
+    protected Reference<College> destinationCollegeReference;
+
+
+    MinorRequest(String title, String body, Reference<Student> sender, Reference<College> originCollegeReference, Reference<College> destinationCollegeReference) {
+        super(title, body, sender);
+        this.originCollegeReference = originCollegeReference;
+        this.destinationCollegeReference = destinationCollegeReference;
+    }
+
+
+
+    // getters and setters
+
+    public Reference<College> getOriginCollegeReference() {
+        return originCollegeReference;
+    }
+
+    public void setOriginCollegeReference(Reference<College> originCollegeReference) {
+        this.originCollegeReference = originCollegeReference;
+    }
+
+
+    public Reference<College> getDestinationCollegeReference() {
+        return destinationCollegeReference;
+    }
+
+    public void setDestinationCollegeReference(Reference<College> destinationCollegeReference) {
+        this.destinationCollegeReference = destinationCollegeReference;
+    }
+}
