c3166f45747b5ccb3a6853be4ee71b08ec4e8258
xmhpx <mhppaydar@gmail.com>
2022-04-18 19:50:13 +0430

    added field 'String name' to class 'College'

--- src/main/java/models/universityitems/College.java
+++ src/main/java/models/universityitems/College.java
@@ -1,4 +1,16 @@
 package models.universityitems;
 
 public class College {
+    protected String name;
+    
+
+    // getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
