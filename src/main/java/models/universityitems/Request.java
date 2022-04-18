3f449a8d17fcaaa7db2038414373e6b08b73dd40
xmhpx <mhppaydar@gmail.com>
2022-04-18 20:48:33 +0430

    implemented class 'Request'

--- src/main/java/models/universityitems/Request.java
+++ src/main/java/models/universityitems/Request.java
@@ -1,4 +1,33 @@
 package models.universityitems;
 
 public class Request {
+    private String title;
+    private String body;
+
+
+    Request(String title, String body){
+        this.title = title;
+        this.body = body;
+    }
+
+
+
+    // getters and setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
