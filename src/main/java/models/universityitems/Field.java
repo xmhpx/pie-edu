94ae0d28beac2e3546ba890ffe222e277605aee0
xmhpx <mhppaydar@gmail.com>
2022-04-18 20:25:01 +0430

    implemented Field class

--- src/main/java/models/universityitems/Field.java
+++ src/main/java/models/universityitems/Field.java
@@ -1,4 +1,34 @@
 package models.universityitems;
 
+import java.lang.ref.Reference;
+
 public class Field {
+    protected String name;
+    private Reference<College> collegeReference;
+
+
+    Field(String name, Reference<College> collegeReference){
+        this.name = name;
+        this.collegeReference = collegeReference;
+    }
+
+
+
+    // getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Reference<College> getCollegeReference() {
+        return collegeReference;
+    }
+
+    public void setCollegeReference(Reference<College> collegeReference) {
+        this.collegeReference = collegeReference;
+    }
 }
