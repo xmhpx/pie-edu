48bb6f15f43d161b2bff21ec4bb6e51eb1816a36
xmhpx <mhppaydar@gmail.com>
2022-04-18 18:02:22 +0430

    added goToHomePage method to StudentHomePageController (don't forget to update menus in other pages aswell

--- src/main/java/StudentHomePageController.java
+++ src/main/java/StudentHomePageController.java
@@ -1,2 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class StudentHomePageController {
+
+    @FXML
+    AnchorPane anchorPane;
+
+
+    void changeScene(Scene scene){
+        Stage primaryStage = (Stage) anchorPane.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public void goToHomePage(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("studentHomePage.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        changeScene(scene);
+    }
 }
