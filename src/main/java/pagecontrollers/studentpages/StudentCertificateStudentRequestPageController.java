e793505ef457919312640bf80594f2cd5321e0aa
xmhpx <mhppaydar@gmail.com>
2022-04-19 20:13:19 +0430

    added a table to 'studentCertificateStudentRequestPage.fxml'

--- src/main/java/pagecontrollers/studentpages/StudentCertificateStudentRequestPageController.java
+++ src/main/java/pagecontrollers/studentpages/StudentCertificateStudentRequestPageController.java
@@ -1,4 +1,26 @@
 package pagecontrollers.studentpages;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import logic.Backend;
+import models.universityitems.requests.CertificateStudentRequest;
+import models.universityitems.requests.Request;
+
 public class StudentCertificateStudentRequestPageController extends StudentPageController {
+    @FXML
+    TableView<CertificateStudentRequest> tableView;
+
+    @Override
+    public void initialize(){
+        super.initialize();
+        Backend backend = Backend.getInstance();
+        ObservableList<CertificateStudentRequest> data = tableView.getItems();
+        data.clear();
+        for(Request request : backend.getRequests()){
+            if(request instanceof CertificateStudentRequest){
+                data.add((CertificateStudentRequest) request);
+            }
+        }
+    }
 }
