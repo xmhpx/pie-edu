28f2fa0d3351451170de8d0d89cbfd60201991fe
xmhpx <mhppaydar@gmail.com>
2022-04-19 20:00:41 +0430

    added a table to 'studentRecommendationLetterRequestPage.fxml'

--- src/main/java/pagecontrollers/studentpages/StudentRecommendationLetterRequestPageController.java
+++ src/main/java/pagecontrollers/studentpages/StudentRecommendationLetterRequestPageController.java
@@ -1,4 +1,27 @@
 package pagecontrollers.studentpages;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import logic.Backend;
+import models.universityitems.Course;
+import models.universityitems.requests.RecommendationLetterRequest;
+import models.universityitems.requests.Request;
+
 public class StudentRecommendationLetterRequestPageController extends StudentPageController {
+    @FXML
+    TableView<RecommendationLetterRequest> tableView;
+
+    @Override
+    public void initialize(){
+        super.initialize();
+        Backend backend = Backend.getInstance();
+        ObservableList<RecommendationLetterRequest> data = tableView.getItems();
+        data.clear();
+        for(Request request : backend.getRequests()){
+            if(request instanceof RecommendationLetterRequest){
+                data.add((RecommendationLetterRequest) request);
+            }
+        }
+    }
 }
