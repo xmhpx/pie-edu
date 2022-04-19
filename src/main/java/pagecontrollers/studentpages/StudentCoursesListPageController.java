99b6a3a2e9fbcca808cc941cc394d70ad1e9e65b
xmhpx <mhppaydar@gmail.com>
2022-04-19 16:42:12 +0430

    removed redundant codes used for testing

--- src/main/java/pagecontrollers/studentpages/StudentCoursesListPageController.java
+++ src/main/java/pagecontrollers/studentpages/StudentCoursesListPageController.java
@@ -2,15 +2,10 @@ package pagecontrollers.studentpages;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TableView;
-import javafx.scene.text.Text;
 import logic.Backend;
 import models.universityitems.Course;
 
-import java.io.IOException;
 
 public class StudentCoursesListPageController extends StudentPageController {
 
@@ -24,5 +19,6 @@ public class StudentCoursesListPageController extends StudentPageController {
         ObservableList<Course> data = tableView.getItems();
         data.clear();
         data.addAll(backend.getCourses());
+        
     }
 }
