f03b42d6cd9ccfe6c8dbe995a3e5afd42fd428ed
xmhpx <mhppaydar@gmail.com>
2022-04-19 22:07:06 +0430

    added a table to 'studentTemporaryScoresPage.fxml'

--- src/main/java/pagecontrollers/studentpages/StudentTemporaryScoresPageController.java
+++ src/main/java/pagecontrollers/studentpages/StudentTemporaryScoresPageController.java
@@ -1,4 +1,34 @@
 package pagecontrollers.studentpages;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import logic.Backend;
+import logic.LoggedInUserHolder;
+import models.User;
+import models.student.Student;
+import models.universityitems.Course;
+import models.universityitems.ReportCard;
+import models.universityitems.ReportCardStatus;
+
 public class StudentTemporaryScoresPageController extends StudentPageController {
+    @FXML
+    TableView<ReportCard> tableView;
+
+    @Override
+    public void initialize(){
+        super.initialize();
+        Backend backend = Backend.getInstance();
+        ObservableList<ReportCard> data = tableView.getItems();
+        data.clear();
+        User user = LoggedInUserHolder.getUser();
+        if(user instanceof Student student) {
+            for(int reportCardId : student.getReportCardIds()) {
+                ReportCard reportCard = backend.getReportCard(reportCardId);
+                if(reportCard.getStatus() == ReportCardStatus.TEMPORARILY_SCORED){
+                    data.add(reportCard);
+                }
+            }
+        }
+    }
 }
