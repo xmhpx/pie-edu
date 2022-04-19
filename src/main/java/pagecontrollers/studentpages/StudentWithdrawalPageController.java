cbdf874ac2302b37a483dc3cbf972194aef32226
xmhpx <mhppaydar@gmail.com>
2022-04-19 20:26:30 +0430

    added a table same as in 'studentCertificateStudentRequestPage.fxml' to 'studentWithdrawalPage.fxml'

--- src/main/java/pagecontrollers/studentpages/StudentWithdrawalPageController.java
+++ src/main/java/pagecontrollers/studentpages/StudentWithdrawalPageController.java
@@ -1,4 +1,26 @@
 package pagecontrollers.studentpages;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import logic.Backend;
+import models.universityitems.requests.Request;
+import models.universityitems.requests.WithdrawalRequest;
+
 public class StudentWithdrawalPageController extends StudentPageController {
+    @FXML
+    TableView<WithdrawalRequest> tableView;
+
+    @Override
+    public void initialize(){
+        super.initialize();
+        Backend backend = Backend.getInstance();
+        ObservableList<WithdrawalRequest> data = tableView.getItems();
+        data.clear();
+        for(Request request : backend.getRequests()){
+            if(request instanceof WithdrawalRequest){
+                data.add((WithdrawalRequest) request);
+            }
+        }
+    }
 }
