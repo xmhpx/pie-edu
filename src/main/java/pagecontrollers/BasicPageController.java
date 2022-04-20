aa5adaed48b6b55ea285588a151d80b25d87f49d
xmhpx <mhppaydar@gmail.com>
2022-04-21 01:50:12 +0430

    added 'BasicPageController'

---
+++ src/main/java/pagecontrollers/BasicPageController.java
@@ -0,0 +1,34 @@
+package pagecontrollers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BasicPageController {
+    @FXML
+    AnchorPane anchorPane;
+
+
+    protected void goToStudentPage(String str) throws IOException {
+        goToPage("/studentpages/"+str);
+    }
+
+    protected void goToPage(String str) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(str));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        changeScene(scene);
+    }
+
+    protected void changeScene(Scene scene){
+        Stage primaryStage = (Stage) anchorPane.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
--- src/main/java/pagecontrollers/studentpages/StudentPageController.java
+++ src/main/java/pagecontrollers/studentpages/StudentPageController.java
@@ -2,21 +2,17 @@ package pagecontrollers.studentpages;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
+import pagecontrollers.BasicPageController;
 
 import java.io.IOException;
 import java.util.Calendar;
 
-public class StudentPageController {
+public class StudentPageController extends BasicPageController {
 
     @FXML
     MenuItem coursesListMenuItem;
@@ -78,8 +74,6 @@ public class StudentPageController {
     @FXML
     Text UIEmailText;
 
-    @FXML
-    AnchorPane anchorPane;
 
     @FXML
     public void initialize() {
@@ -111,112 +105,91 @@ public class StudentPageController {
     }
 
 
-    void goToStudentPage(String str) throws IOException {
-        goToPage("/studentpages/"+str);
-    }
-
-
-    void goToPage(String str) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(str));
-        Parent root = loader.load();
-
-        Scene scene = new Scene(root);
-        changeScene(scene);
-    }
-
-
-    void changeScene(Scene scene){
-        Stage primaryStage = (Stage) anchorPane.getScene().getWindow();
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-
     @FXML
-    void goToStudentHomePage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentHomePage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentHomePage.fxml");
     }
 
 
     @FXML
-    void goToStudentCoursesListPage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentCoursesListPage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentCoursesListPage.fxml");
     }
 
 
     @FXML
-    void goToStudentProfessorsListPage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentProfessorsListPage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentProfessorsListPage.fxml");
     }
 
 
     @FXML
-    void goToStudentWeeklySchedulePage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentWeeklySchedulePage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentWeeklySchedulePage.fxml");
     }
 
 
     @FXML
-    void goToStudentExamsListPage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentExamsListPage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentExamsListPage.fxml");
     }
 
 
     @FXML
-    void goToStudentRecommendationLetterRequestPage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentRecommendationLetterRequestPage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentRecommendationLetterRequestPage.fxml");
     }
 
 
     @FXML
-    void goToStudentCertificateStudentRequestPage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentCertificateStudentRequestPage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentCertificateStudentRequestPage.fxml");
     }
 
 
     @FXML
-    void goToStudentMinorPage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentMinorPage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentMinorPage.fxml");
     }
 
 
     @FXML
-    void goToStudentWithdrawalPage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentWithdrawalPage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentWithdrawalPage.fxml");
     }
 
 
     @FXML
-    void goToStudentTemporaryScoresPage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentTemporaryScoresPage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentTemporaryScoresPage.fxml");
     }
 
 
     @FXML
-    void goToStudentEducationStatusPage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentEducationStatusPage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentEducationStatusPage.fxml");
     }
 
 
     @FXML
-    void goToStudentViewProfilePage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentViewProfilePage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentViewProfilePage.fxml");
     }
 
 
     @FXML
-    void goToStudentEditPasswordPage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentEditPasswordPage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentEditPasswordPage.fxml");
     }
 
 
     @FXML
-    void goToStudentEditProfilePage(ActionEvent actionEvent) throws IOException {
+    protected void goToStudentEditProfilePage(ActionEvent actionEvent) throws IOException {
         goToStudentPage("studentEditProfilePage.fxml");
     }
 
     @FXML
-    public void logOutButtonClick(MouseEvent mouseEvent) {
+    protected void logOutButtonClick(MouseEvent mouseEvent) {
 
     }
 }
