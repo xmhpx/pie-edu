9fed934d632452c9bd2a0489dedadb88f38c8832
xmhpx <mhppaydar@gmail.com>
2022-04-21 18:58:33 +0430

    sorted exam lists by their date

--- src/main/java/pagecontrollers/studentpages/StudentExamsListPageController.java
+++ src/main/java/pagecontrollers/studentpages/StudentExamsListPageController.java
@@ -9,6 +9,8 @@ import models.User;
 import models.student.Student;
 import models.universityitems.Course;
 
+import java.util.Comparator;
+
 public class StudentExamsListPageController extends StudentPageController {
 
     public static final String fxmlFileName = "studentExamsListPage.fxml";
@@ -28,5 +30,16 @@ public class StudentExamsListPageController extends StudentPageController {
                 data.add(backend.getCourse(courseId));
             }
         }
+
+        Comparator<Course> courseComparator = new Comparator<Course>() {
+            @Override
+            public int compare(Course o1, Course o2) {
+                String examDate1 = o1.getExamDate();
+                String examDate2 = o2.getExamDate();
+                return examDate1.compareTo(examDate2);
+            }
+        };
+
+        data.sort(courseComparator);
     }
 }
