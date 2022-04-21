763206ea1913e99b62eb44949d3df47997b9c646
xmhpx <mhppaydar@gmail.com>
2022-04-21 19:29:33 +0430

    modified 'studentDormRequestPage.fxml' and its controller to function

--- src/main/java/pagecontrollers/studentpages/StudentDormRequestPageController.java
+++ src/main/java/pagecontrollers/studentpages/StudentDormRequestPageController.java
@@ -11,19 +11,17 @@ import logic.Backend;
 import logic.LoggedInUserHolder;
 import models.User;
 import models.student.Student;
-import models.universityitems.Field;
-import models.universityitems.requests.MinorRequest;
+import models.universityitems.requests.DormRequest;
 import models.universityitems.requests.Request;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class StudentDormRequestPageController extends StudentPageController {
 
     public static final String fxmlFileName = "studentDormRequestPage.fxml";
 
     @FXML
-    TableView<MinorRequest> tableView;
+    TableView<DormRequest> tableView;
 
     @FXML
     TextField titleTextField;
@@ -32,32 +30,27 @@ public class StudentDormRequestPageController extends StudentPageController {
     TextField bodyTextField;
 
     @FXML
-    TextField destinationCollegeIdTextField;
-
-    @FXML
-    TextField minorFieldIdTextField;
+    Button addRequestButton;
 
     @FXML
     Text errorText;
-
-    @FXML
-    Button addRequestButton;
 
 
     @Override
     public void initialize(){
         super.initialize();
         Backend backend = Backend.getInstance();
-        ObservableList<MinorRequest> data = tableView.getItems();
+        ObservableList<DormRequest> data = tableView.getItems();
         data.clear();
         User user = LoggedInUserHolder.getUser();
         if(user instanceof Student student) {
             for(int requestId : student.getRequestIds()) {
                 Request request = backend.getRequest(requestId);
-                if(request instanceof MinorRequest)data.add((MinorRequest) request);
+                if(request instanceof DormRequest)data.add((DormRequest) request);
             }
         }
     }
+
 
     @FXML
     void addRequestButtonOnAction(ActionEvent actionEvent) {
@@ -65,54 +58,18 @@ public class StudentDormRequestPageController extends StudentPageController {
 
         String title = titleTextField.getText();
         String body = bodyTextField.getText();
-        int destinationCollegeId;
-        int minorFieldId;
 
         if(title.equals("")){
             error("empty title is not allowed");
             return;
         }
 
-        try {
-            destinationCollegeId = Integer.parseInt(destinationCollegeIdTextField.getText());
-        }
-        catch (NumberFormatException numberFormatException){
-            error("destination college id must be an integer");
-            return;
-        }
+        DormRequest request = new DormRequest(title, body, LoggedInUserHolder.getUser().getId());
+        backend.addToRequests(request);
+        Student student = backend.getStudent(request.getSenderId());
+        student.addToRequest(request.getId());
 
-        try {
-            minorFieldId = Integer.parseInt(minorFieldIdTextField.getText());
-        }
-        catch (NumberFormatException numberFormatException){
-            error("minor field id must be an integer");
-            return;
-        }
-
-        if(!backend.hasCollege(destinationCollegeId)){
-            error("destination college doesn't exist");
-            return;
-        }
-        if(!backend.hasField(minorFieldId)){
-            error("minor field doesn't exist");
-            return;
-        }
-
-        ArrayList<Field> fields = backend.getFields();
-
-        for(Field field : fields){
-            if(field.getCollegeId() == destinationCollegeId && field.getId() == minorFieldId){
-                MinorRequest request = new MinorRequest(title, body, LoggedInUserHolder.getUser().getId(), destinationCollegeId, minorFieldId);
-                backend.addToRequests(request);
-                Student student = backend.getStudent(request.getSenderId());
-                student.addToRequest(request.getId());
-
-                reload();
-                return;
-            }
-        }
-
-        error("the destination college doesn't have that minor field");
+        reload();
     }
 
     private void error(String error){
@@ -124,8 +81,6 @@ public class StudentDormRequestPageController extends StudentPageController {
         errorText.setText("");
         titleTextField.setText("");
         bodyTextField.setText("");
-        destinationCollegeIdTextField.setText("");
-        minorFieldIdTextField.setText("");
     }
 
     private void reload(){
