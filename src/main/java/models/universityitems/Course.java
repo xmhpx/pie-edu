66b5fbebd75124317c5adbf72a151874262ad9b8
xmhpx <mhppaydar@gmail.com>
2022-04-21 15:27:06 +0430

    added 'Time' class

--- src/main/java/models/universityitems/Course.java
+++ src/main/java/models/universityitems/Course.java
@@ -1,7 +1,5 @@
 package models.universityitems;
 
-import models.professor.Professor;
-
 import java.util.ArrayList;
 
 public class Course {
---
+++ src/main/java/models/universityitems/Time.java
@@ -0,0 +1,45 @@
+package models.universityitems;
+
+public class Time {
+    private int hour;
+    private int minute;
+
+
+    public Time(int hour, int minute) {
+        setHour(hour);
+        setMinute(minute);
+    }
+
+
+    @Override
+    public String toString(){
+        return hour + ":" + minute;
+    }
+
+
+    // getters and setters
+
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        if(hour < 0 || 23 < hour){
+            throw new IllegalArgumentException("in Time class, hour is not in range");
+        }
+        this.hour = hour;
+    }
+
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        if(minute < 0 || 59 < minute){
+            throw new IllegalArgumentException("in Time class, minute is not in range");
+        }
+        this.minute = minute;
+    }
+}
