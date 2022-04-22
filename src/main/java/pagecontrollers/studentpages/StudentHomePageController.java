efa8ae41d338675440d5bec820cd74008e4eb6b9
xmhpx <mhppaydar@gmail.com>
2022-04-22 18:11:33 +0430

    fixed some bugs causing texts in 'studentHomePage.fxml' to not initiate

--- src/main/java/pagecontrollers/studentpages/StudentHomePageController.java
+++ src/main/java/pagecontrollers/studentpages/StudentHomePageController.java
@@ -1,12 +1,48 @@
 package pagecontrollers.studentpages;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import logic.Backend;
+import logic.LoggedInUserHolder;
+import models.User;
+import models.student.Student;
+
 public class StudentHomePageController extends StudentPageController {
 
     public static final String fxmlFileName = "studentHomePage.fxml";
 
+
+    @FXML
+    Text educationStatusText;
+
+    @FXML
+    Text supervisorText;
+
+    @FXML
+    Text registrationLicenseText;
+
+    @FXML
+    Text registrationTimeText;
+
+
     @Override
     public void initialize(){
         super.initialize();
+        User user = LoggedInUserHolder.getUser();
+        if(user instanceof Student student) {
+            educationStatusText.setText("Education Status: " + student.getEducationStatus());
+
+            int supervisorId = student.getSupervisorId();
+            if(Backend.getInstance().hasProfessor(supervisorId)){
+                supervisorText.setText("Supervisor Name: " + Backend.getInstance().getProfessor(supervisorId).getName());
+            }
+            else{
+                supervisorText.setText("Supervisor Name: N/A");
+            }
+
+            registrationLicenseText.setText("Registration License: " + student.getRegistrationLicense());
+            registrationTimeText.setText("Registration Time: " + student.getRegistrationTime());
+        }
     }
 
 }
