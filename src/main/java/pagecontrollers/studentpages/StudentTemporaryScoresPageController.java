087a65fba2842ca0a7f7ceabefe0e59f5d268967
xmhpx <mhppaydar@gmail.com>
2022-04-26 00:53:35 +0430

    added ability to edit 'ReportCard.objection' in 'studentTemporaryScoresPages.fxml'

--- src/main/java/pagecontrollers/studentpages/StudentTemporaryScoresPageController.java
+++ src/main/java/pagecontrollers/studentpages/StudentTemporaryScoresPageController.java
@@ -1,14 +1,19 @@
 package pagecontrollers.studentpages;
 
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import logic.Backend;
 import logic.LoggedInUserHolder;
 import models.User;
 import models.student.Student;
 import models.universityitems.ReportCard;
 import models.universityitems.ReportCardStatus;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -43,5 +48,25 @@ public class StudentTemporaryScoresPageController extends StudentPageController 
             log.error("logged in user is not a student");
             throw new IllegalStateException("logged in user is not a student");
         }
+
+        tableView.setEditable(true);
+
+        TableColumn<ReportCard, String> objectionColumn = new TableColumn<>("Objection");
+        objectionColumn.setMaxWidth(200);
+        objectionColumn.setPrefWidth(100);
+        objectionColumn.setCellValueFactory(new PropertyValueFactory<>("objection"));
+        objectionColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        objectionColumn.setOnEditCommit(event -> {
+            ReportCard reportCard = event.getRowValue();
+            reportCard.setObjection(event.getNewValue());
+        });
+
+
+        TableColumn<ReportCard, String> responseColumn = new TableColumn<>("Response");
+        responseColumn.setMaxWidth(200);
+        responseColumn.setPrefWidth(100);
+
+        tableView.getColumns().add(objectionColumn);
+        tableView.getColumns().add(responseColumn);
     }
 }
