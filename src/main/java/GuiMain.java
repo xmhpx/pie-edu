944cde02ad614ffa3a39f63fb0b1e2f2d2456b2a
xmhpx <mhppaydar@gmail.com>
2022-04-26 07:31:57 +0430

    <<<uploaded version>>>

--- src/main/java/GuiMain.java
+++ src/main/java/GuiMain.java
@@ -10,9 +10,6 @@ import org.apache.log4j.Logger;
 import java.io.IOException;
 
 
-//// TODO maybe add ability to edi 'homeAddress' and 'postalCode'
-//// TODO maybe make a page to add/edit/remove class times
-
 
 public class GuiMain extends Application{
     private static final Logger log = LogManager.getLogger(GuiMain.class);
